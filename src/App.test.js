d49fa0f82fe7b0c641a0f1bafc1adc489daa7730
Santanu Biswas <shanpro.2015@gmail.com>
2024-05-03 00:00:16 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Learn CICD text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn CICD/i);
   expect(linkElement).toBeInTheDocument();
 });
