febf8392cbc2d82669193fc4fb985f627fe0883c
Santanu Biswas <shanpro.2015@gmail.com>
2024-05-20 17:20:43 +0530

    Contact us unit test

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,8 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="App">
-      <p>Contact Us Page</p>
+      <p>Contact Us</p>
+      <p>Thank you for reaching out to us</p>
     </div>
   );
 }
