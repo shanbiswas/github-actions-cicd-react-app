e426fc37f098e00256c0caad1c2139c8d09c5b2c
Santanu Biswas <shanpro.2015@gmail.com>
2024-05-21 10:25:23 +0530

    changes

--- src/Contact.test.js
+++ src/Contact.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import Contact from '../Contact';
+import Contact from './Contact';
 
 test(`renders 'Contact Us' text`, () => {
   render(<Contact />);
