e111cb42c900723c8d412f4f46089181ae59b916
Santanu Biswas <shanpro.2015@gmail.com>
2024-05-30 22:09:22 +0530

    changes

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -5,7 +5,7 @@ const AboutUs = () => {
     <div className="App">
       <p>About Us Page CHANGED TEXT</p>
       <p>Random text, modified text</p>
-      <p>Welcome</p>
+      <p>Welcome!</p>
     </div>
   );
 }
