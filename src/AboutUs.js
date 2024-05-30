1563275158504d29c8398b2fe272ff6d28c15fba
Santanu Biswas <shanpro.2015@gmail.com>
2024-05-30 21:09:33 +0530

    about us page text change

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -4,7 +4,7 @@ const AboutUs = () => {
   return (
     <div className="App">
       <p>About Us Page CHANGED TEXT</p>
-      <p>Random text</p>
+      <p>Random text, modified text</p>
       <p>Welcome</p>
     </div>
   );
